611a337055070addf191a76527357aea637d6554
Ricardo Alvarez <ralvarezcm@gmail.com>
2022-02-04 12:04:54 -0500

    feat: updated package scripts

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const pool = require('./pool');
 
 const PORT = process.env.PORT || 3000;
 
+const connectionString = process.env.DATABASE_URL;
+
 pool
   .connect({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    connectionString,
   })
   .then(async () => {
     server().listen(PORT, () => {
